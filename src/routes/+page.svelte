6ec3e79d67c7654b0a910489aff19e2c1a1ff58c
Walker <walker.b.lockard@gmail.com>
2024-10-08 15:17:31 -0500

    updated readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,24 +41,30 @@
 </script>
 
 <h1>Tanstack Table v8 + Svelte 5 Reference</h1>
+<hr />
 
 <h2>Overview</h2>
+<hr />
 
 <p>
-	The official Svelte 5 adpater for TanStack Table will release alongside the v9 of the TanStack
-	Table library. As of writing this, TanStack Table v9 is still under development. In the meantime,
-	you can use Svelte 5 with TanStack Table by creating your own "adapter" in your Svelte 5 project
-	for TanStack Table v8, which is the current production version of TansTack Table.
+	The <code>@tanstack/table-svelte</code> adapter doesn't support Svelte 5 because it uses the
+	<code>internal</code> package, which is being deprecated. This project is a reference for how to implement
+	TanStack Table v8 with Svelte 5.
 </p>
 
 <p>
-	You can use this site and its repository on github as a reference for implementing your own
-	adapter.
+	In the not-to-distant future, the official Svelte 5 adpater for TanStack Table will release
+	alongside the v9 of the TanStack Table library. As of writing this, TanStack Table v9 is still
+	under development.
 </p>
 
-<p>This site is rudamentary on purpose.</p>
+<p>
+	You can use this site and its repository on <a href={PUBLIC_GITHUB_REPO_URL}>GitHub</a> as a reference
+	for implementing your own adapter.
+</p>
 
 <h2>Examples</h2>
+<hr />
 <table>
 	<thead>
 		<tr>
@@ -85,16 +91,31 @@
 </table>
 
 <h2>Questions you may have</h2>
+<hr />
 
 <h3>Why not just use the alpha (v9) branch?</h3>
 
 <p>
-	Alpha is under development. It changes very frequently, and therefor breaks very frequently. While
-	things are so volatile, maintainers and contributors can't be expected to support it.
+	Alpha is under development. It changes very frequently, and therefore breaks very frequently.
+	While things are so volatile, maintainers and contributors can't be expected to support it.
 </p>
 
 <p>
 	This applies to both <code>@tanstack/table-core</code> and <code>@tanstack/table-svelte</code>.
+</p>
+
+<h3>How can I get help?</h3>
+
+<p>
+	You can ask questions by submitting an issue in the <a href={PUBLIC_GITHUB_REPO_URL}
+		>GitHub repository</a
+	>. Please do try and figure things out on your own first, though.
+</p>
+
+<p style="font-weight: bold;">
+	Please also DO NOT submit an issue that you have with this project on the official TanStack Table
+	repository. ONLY submit an issue there if you're sure that it's a problem with their library, and
+	not this project or your own implementation.
 </p>
 
 <style>
@@ -108,5 +129,9 @@
 	td {
 		border: 1px solid black;
 		padding: 8px;
+	}
+
+	h2 {
+		margin-top: 2rem;
 	}
 </style>
